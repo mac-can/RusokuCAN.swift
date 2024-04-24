5b58cbd5adc7a0ad7c8bb659d44a6a5bd0f9937f
Uwe McVogt <mac@uv-software.de>
2024-04-24 15:59:46 +0200

    Update Swift package dependency
    Latest is Greatest !

--- SwiftCAN/main.swift
+++ SwiftCAN/main.swift
@@ -2,13 +2,13 @@
 //
 //  main.swift
 //  SwiftCAN-TouCAN
-//  Copyright (c) 2021-2023 Uwe Vogt, UV Software, Berlin
+//  Copyright (c) 2021-2024 Uwe Vogt, UV Software, Berlin
 //
 import Foundation
 import TouCAN
 
 print("\(try CanApi.GetVersion())")
-print("Copyright (c) 2021-2023 Uwe Vogt, UV Software, Berlin")
+print("Copyright (c) 2021-2024 Uwe Vogt, UV Software, Berlin")
 print("")
 print("This program is free software: you can redistribute it and/or modify")
 print("it under the terms of the GNU General Public License as published by")
@@ -21,7 +21,7 @@ print("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the")
 print("GNU General Public License for more details.")
 print("")
 print("You should have received a copy of the GNU General Public License")
-print("along with this program.  If not, see <http://www.gnu.org/licenses/>.")
+print("along with this program.  If not, see <https://www.gnu.org/licenses/>.")
 print("")
 
 let can = CanApi()
